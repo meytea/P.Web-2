25cc1bea759046bef27b8533d3831cd803bf4e64
meytea <139698549+meytea@users.noreply.github.com>
2024-09-10 09:49:17 +0700

    Add files via upload

---
+++ pweb2/Jobsheet2/tugas.php
@@ -0,0 +1,21 @@
+<?php
+    class Dosen {
+        public $nama;
+        public $nip;
+        public $mataKuliah;
+
+        public function tampilkanDosen(){
+            return "Nama Dosen   : $this->nama <br>
+                    NIP          : $this->nip <br>
+                    Mata Kuliah  : $this->mataKuliah";
+        }
+    }
+
+    //Instansiasi Object
+    $dosen1 = new Dosen ();
+    $dosen1->nama = "Andi Suripto";
+    $dosen1->nip = "10283216140946";
+    $dosen1->mataKuliah ="Matematika";
+
+    echo $dosen1->tampilkanDosen();
+?>
